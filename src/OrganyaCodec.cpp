33be3cf6fba9b31c9ff81dba58d8763fe81a9c0c
Alwin Esch <alwin.esch@web.de>
2019-09-14 03:04:05 +0200

    rework addon to fix crashes and remove p8-platform

--- src/OrganyaCodec.cpp
+++ src/OrganyaCodec.cpp
@@ -1,4 +1,5 @@
 /*
+ *      Copyright (C) 2019 Team Kodi
  *      Copyright (C) 2014 Arne Morten Kvarving
  *
  *  This Program is free software; you can redistribute it and/or modify
@@ -17,61 +18,44 @@
  *
  */
 
-#include "RingBuffer.h"
+#include "OrganyaCodec.h"
 
-#include "decoder.h"
-#include "organya.h"
-
-#include <algorithm>
-#include <iostream>
-
-#include <kodi/addon-instance/AudioDecoder.h>
-#include <kodi/Filesystem.h>
-
-
-class ATTRIBUTE_HIDDEN COrganyaCodec : public kodi::addon::CInstanceAudioDecoder
+COrganyaCodec::~COrganyaCodec()
 {
-public:
-  COrganyaCodec(KODI_HANDLE instance) : CInstanceAudioDecoder(instance) {};
-  ~COrganyaCodec() override;
-  bool Init(const std::string& filename, unsigned int filecache, int& channels, int& samplerate,
-            int& bitspersample, int64_t& totaltime,
-            int& bitrate, AEDataFormat& format,
-            std::vector<AEChannel>& channellist) override;
-  int ReadPCM(uint8_t* buffer, int size, int& actualsize) override;
-  int64_t Seek(int64_t time) override;
-  bool ReadTag(const std::string& file, std::string& title, std::string& artist, int& length) override { return true; }
-
-private:
-  org_decoder_t* tune;
-  int64_t length;
-  int64_t pos;
-  CRingBuffer sample_buffer;
-};
-
+  org_decoder_destroy(m_tune);
+}
 
 bool COrganyaCodec::Init(const std::string& strFile, unsigned int filecache, int& channels,
                          int& samplerate, int& bitspersample, int64_t& totaltime,
                          int& bitrate, AEDataFormat& format, std::vector<AEChannel>& channelinfo)
 {
-  sample_buffer.Create(4096);
-  kodi::vfs::CFile file;
-  file.OpenFile(strFile);
-  if (!file.OpenFile(strFile, 0))
+  if (!m_file.OpenFile(strFile, 0))
+  {
+    kodi::Log(ADDON_LOG_ERROR, "Failed to open: '%s'", strFile.c_str());
     return false;
-  std::string temp = kodi::GetSettingString("__addonpath__") + "/resources/samples";
-  tune = org_decoder_create(&file, temp.c_str(), 1);
-  tune->state.sample_rate = 48000;
-  totaltime = org_decoder_get_total_samples(tune)*1000/48000;
-  length = totaltime/1000*48000*4;
+  }
+
+  std::string temp = kodi::GetAddonPath("resources/samples");
+  m_tune = org_decoder_create(&m_file, temp.c_str(), !m_cfgLoopIndefinitely ? 1 : m_cfgLoopCnt);
+  if (!m_tune)
+  {
+    kodi::Log(ADDON_LOG_ERROR, "Failed to create organya decoder");
+    return false;
+  }
+
+  m_tune->state.sample_rate = m_cfgSampleRate;
+  m_length = org_decoder_get_total_samples(m_tune);
+  m_fadeTime = mul_div(m_cfgFadeTime, m_cfgSampleRate, 1000);
+  m_samplesDecoded = 0;
+
+  totaltime = m_length * 1000 / m_cfgSampleRate;
   format = AE_FMT_S16NE;
   channelinfo = { AE_CH_FL, AE_CH_FR, AE_CH_NULL };
   channels = 2;
   bitspersample = 16;
   bitrate = 0.0;
-  samplerate = 48000;
+  samplerate = m_cfgSampleRate;
 
-  file.Close();
   Seek(0);
 
   return true;
@@ -79,50 +63,62 @@ bool COrganyaCodec::Init(const std::string& strFile, unsigned int filecache, int
 
 int COrganyaCodec::ReadPCM(uint8_t* pBuffer, int size, int &actualsize)
 {
-  if (pos >= length*48000*4/1000)
-    return 1;
+  if (!m_cfgLoopIndefinitely && m_samplesDecoded > m_length)
+    return -1;
 
-  if (sample_buffer.getMaxReadSize() == 0) {
-    int16_t temp[1024*2]; 
-    int64_t written=1024;
-    written = org_decode_samples(tune, temp, written);
-    if (written == 0)
-      return 1;
-    sample_buffer.WriteData((const char*)temp, written*4);
+  int16_t* ptr = reinterpret_cast<int16_t*>(pBuffer);
+
+  unsigned int samples_todo = size / 2 / sizeof(int16_t);
+  int64_t samplesDecodedFrame = org_decode_samples(m_tune, ptr, samples_todo);
+  if (samplesDecodedFrame)
+  {
+    if (!m_cfgLoopIndefinitely)
+    {
+      int64_t fade_start = m_samplesDecoded;
+      int64_t fade_end = m_samplesDecoded += samplesDecodedFrame;
+
+      if (fade_start > m_length - m_fadeTime)
+      {
+        for (int n = fade_start; n < fade_end; ++n)
+        {
+          int bleh = m_length - n;
+          ptr[0] = mul_div(ptr[0], bleh, m_fadeTime);
+          ptr[1] = mul_div(ptr[1], bleh, m_fadeTime);
+          ptr += 2;
+        }
+      }
+    }
+
+    actualsize = samplesDecodedFrame * 2 * sizeof(int16_t);
+  }
+  else
+  {
+    actualsize = 0;
+    return -1;
   }
 
-  int tocopy = std::min(size, (int)sample_buffer.getMaxReadSize());
-  sample_buffer.ReadData((char*)pBuffer, tocopy);
-  pos += tocopy;
-  actualsize = tocopy;
   return 0;
 }
 
 int64_t COrganyaCodec::Seek(int64_t time)
 {
-  pos = time*48000*4/1000;
-
-  org_decoder_seek_sample(tune, pos/4);
+  int64_t pos = time * m_cfgSampleRate / 1000;
+  org_decoder_seek_sample(m_tune, pos);
+  m_samplesDecoded = pos;
   
   return time;
-}
-
-COrganyaCodec::~COrganyaCodec()
-{
-  org_decoder_destroy(tune);
 }
 
 class ATTRIBUTE_HIDDEN CMyAddon : public kodi::addon::CAddonBase
 {
 public:
-  CMyAddon() { }
-  virtual ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
+  CMyAddon() = default;
+  ~CMyAddon() override = default;
+  ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
   {
     addonInstance = new COrganyaCodec(instance);
     return ADDON_STATUS_OK;
   }
-
-  virtual ~CMyAddon() {}
 };
 
 ADDONCREATOR(CMyAddon); // Don't touch this!
